0a58e51e5a8a18690c73cbca6b85ba36a7868ea8
tenoken <shiroiparadina@gmail.com>
2023-12-15 15:21:37 -0300

    refactor: template class

--- src/test/Template.java
+++ src/test/Template.java
@@ -13,62 +13,52 @@ import java.util.List;
 public class Template extends ReportBase implements IReportFormatter {
 
     private Document htmlDoc;
+
     public Template(String templatePath) throws IOException {
-
-        if(templatePath == null)
+        if (templatePath == null) {
             throw new IllegalArgumentException("The path must have a valid value.");
-
-        try {
-            this.htmlDoc = loadHTMLTemplate(templatePath);
-        } catch (IOException e) {
-            throw new IOException("The template file could not be found.");
         }
+        this.htmlDoc = loadHTMLTemplate(templatePath);
     }
 
     private Document loadHTMLTemplate(String templatePath) throws IOException {
-
-        Document doc = Jsoup.parse(new File(templatePath), "UTF-8");
-        return doc;
+        return Jsoup.parse(new File(templatePath), "UTF-8");
     }
 
     @Override
     public ArrayList<String> format(List<SheetCell> sheetCells) {
-
-        var report = new ArrayList<String>();
+        ArrayList<String>  report = new ArrayList<>();
 
         var tableMap = super.groupByLine(sheetCells);
 
-        for (var key : tableMap.keySet()
-        ) {
-            var line = "";
-            var rows = tableMap.get(key);
-
-            line += formatRowId(key, "lineId");
-
-            for (var row: rows
-                 ) {
-                try {
-                    line += formatRow(row);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            // append line to container
+        for (var key : tableMap.keySet()) {
+            var line = formatLine(tableMap.get(key), key);
             appendRowToElement("lineContainer", key, line);
-
             report.add(line);
         }
+
         removeTemplateNodes();
-        return  report;
+        return report;
+    }
+
+    private String formatLine(List<SheetCell> rows, int key) {
+        var line = new StringBuilder();
+        line.append(formatRowId(key, "lineId"));
+
+        for (var row : rows) {
+            try {
+                line.append(formatRow(row));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return line.toString();
     }
 
     private void appendRowToElement(String key, int rowId, String line) {
-
         var clone = htmlDoc.getElementById(key).clone();
         clone.append(line);
         clone.id(String.valueOf(rowId));
-
         clone.appendTo(htmlDoc.getElementById("content"));
     }
 
@@ -78,11 +68,30 @@ public class Template extends ReportBase implements IReportFormatter {
         htmlDoc.getElementById("lineContainer").remove();
     }
 
-    public String formatRowId(int id, String fieldName){
+    public String formatRowId(int id, String fieldName) {
+        Element element = htmlDoc.getElementById("lineId").clone();
+        updateNodeText(element, fieldName, String.valueOf(id));
 
-        Element element = htmlDoc.getElementById("lineId");
-        element =  element.clone();
+        replaceKeyByValue(element, fieldName, String.valueOf(id));
 
+        return element.html();
+    }
+
+    public String formatRow(SheetCell row) throws IllegalAccessException {
+        var fields = getTemplateFields();
+        Element element = htmlDoc.getElementById("column").clone();
+
+        for (var field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(row);
+
+            replaceKeyByValue(element, field.getName(), value.toString());
+        }
+
+        return element.html();
+    }
+
+    private void replaceKeyByValue(Element element, String  fieldName, String value) {
         if (element != null && element.children() != null){
 
             Element finalElment = element;
@@ -92,64 +101,32 @@ public class Template extends ReportBase implements IReportFormatter {
                     return;
 
                 if(c.toString().contains("{{"+ fieldName +"}}")){
-                    updateNodeText(((Element) c), fieldName, String.valueOf(id));
+                    updateNodeText(((Element) c), fieldName, value);
                 }
             });
         }
-
-        return element.html();
-    }
-
-    public String formatRow(SheetCell row) throws IllegalAccessException {
-
-        var fields = getTemplateFields();
-        Element element = htmlDoc.getElementById("column");;
-
-        element =  element.clone();
-
-        for (var field : fields
-             ) {
-
-            field.setAccessible(true);
-            Object value = field.get(row);
-
-            if (element != null && element.children() != null){
-
-                Element finalElment = element;
-                element.childNodes().forEach(c -> {
-
-                    if(c == null)
-                        return;
-
-                    if(c.toString().contains("{{"+ field.getName() +"}}")){
-                        updateNodeText(((Element) c), field.getName(), value.toString());
-                    }
-                });
-            }
-        }
-
-        return element.html();
     }
 
     private ArrayList<Field> getTemplateFields() {
         var fields = SheetCell.class.getDeclaredFields();
         ArrayList<Field> fieldsFound = new ArrayList<>();
 
-        for (var field : fields
-        ) {
-            if(htmlDoc.text().contains(field.getName()))
+        for (var field : fields) {
+            if (htmlDoc.text().contains(field.getName())) {
                 fieldsFound.add(field);
+            }
         }
         return fieldsFound;
     }
 
-    private void updateNodeText(Element element, String oldText, String newtext){
+    private void updateNodeText(Element element, String oldText, String newtext) {
+        var node = element.textNodes().stream()
+                .filter(c -> c.toString().contains("{{" + oldText + "}}"))
+                .findFirst();
 
-        var node = element.textNodes().stream().filter(c -> c.toString().contains("{{"+ oldText +"}}")).findFirst();
-
-        if(node.isPresent()){
-            String updatedText = node.get().text().replace("{{" + oldText + "}}", newtext);
-            node.get().text(updatedText);
-        }
+        node.ifPresent(n -> {
+            String updatedText = n.text().replace("{{" + oldText + "}}", newtext);
+            n.text(updatedText);
+        });
     }
 }
